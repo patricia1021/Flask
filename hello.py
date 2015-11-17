54abc5e8d68e024cd62510794e7cb53768181f73
Patrícia Guimarães <deud.patricia@yahoo.com.br>
2015-11-17 08:59:10 +0000

    test
    1

---
+++ hello.py
@@ -0,0 +1,44 @@
+from flask import Flask, url_for
+app = Flask(__name__)
+
+#@app.route('/')
+#def hello_world():
+#    return 'Hello '
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello World'
+
+#@app.route('/user/<username>')
+#def show_user_profile(username):
+    # show the user profile for that user
+#    return 'Hello %s' % username
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+@app.route('/login')
+def login(): pass
+
+@app.route('/user/<username>')
+def profile(username): pass
+
+with app.test_request_context():
+	print url_for('index')
+	print url_for('login')
+	print url_for('login', next='/')
+	print url_for('profile', username='John Doe')
+
+if __name__ == '__main__':
+   app.run()
